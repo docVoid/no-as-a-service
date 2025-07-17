eeb5a4ce56d4e8791a0d40fd8f0edc8abfc3895d
Robin-Ahmeti <robinsadik.ahmeti@gmail.com>
2025-07-17 13:46:40 +0200

    project structure

---
+++ models/NoResponse.cs
@@ -0,0 +1,7 @@
+namespace NoService.Model {
+    class NoResponse
+    {
+        public string message { get; set; }
+        public string langugage { get; set; }
+        public string style { get; set; }
+    }
